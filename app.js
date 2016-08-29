74a66c1616f12d0387fa609639a9d221dc3dcab6
Troy Carson <troycarson100@gmail.com>
2016-08-29 11:41:08 -0700

    Added spin enemy function, added restart button

--- app.js
+++ app.js
@@ -7,29 +7,52 @@ window.addEventListener("keydown", function(e) {
 }, false);
 /////////////////////////////////////////
 
+
+$btn = $('#startBtn');
 $hero = $('#hero');
 $r1 = $('.r1');
 $r2 = $('.r2');
 $r3 = $('.r3');
 $r4 = $('.r4');
 
+//Individual Enemy Cells
+$e1 = $('#en-a');
+$e2 = $('#en-b');
+$e3 = $('#en-c');
+$e4 = $('#en-d');
+
+$e5 = $('#en-e');
+$e6 = $('#en-f');
+$e7 = $('#en-g');
+$e8 = $('#en-h');
+
+$e9 = $('#en-i');
+$e10 = $('#en-j');
+$e11 = $('#en-k');
+$e12 = $('#en-l');
+
+$e13 = $('#en-m');
+$e14 = $('#en-n');
+$e15 = $('#en-o');
+$e16 = $('#en-p');
+
 //Key Up,Down,Left,Right Hero Functions
 $(document).keydown(function(e){
     if (e.key == "ArrowLeft") {
        $hero.css("background-position", "855px")
-       $r2.css("background", "linear-gradient(to bottom right, teal, yellow)")
+       $r2.css("background", "linear-gradient(to bottom, teal, yellow)")
     }
     else if (e.key == "ArrowUp") {
         $hero.css("background-position", "1306px")
-        $r1.css("background", "linear-gradient(to bottom right, teal, yellow)")
+        $r1.css("background", "linear-gradient(to right, teal, yellow)")
     }
     else if (e.key == "ArrowRight") {
         $hero.css("background-position", "167px")
-        $r3.css("background", "linear-gradient(to bottom right, teal, yellow)")
+        $r3.css("background", "linear-gradient(to bottom, teal, yellow)")
     }
     else if (e.key == "ArrowDown") {
         $hero.css("background-position", "1160px")
-        $r4.css("background", "linear-gradient(to bottom right, teal, yellow)")
+        $r4.css("background", "linear-gradient(to right, teal, yellow)")
     }
 });
 $(document).keyup(function(e){
@@ -49,3 +72,21 @@ $(document).keyup(function(e){
          $r4.removeAttr("style")
     }
 });
+
+
+//Start Enemy Function
+function startEnemy(){
+
+};
+//Enemy Spin
+function spin(){
+  setInterval(function(){
+    $e1.toggleClass('enImg1');
+    setTimeout(function(){
+      $e1.toggleClass('enImg2');
+    }, 100);
+  }, 100);
+}
+
+//Start/Stop Button
+$btn.on('click', spin);
---
+++ readme.md
@@ -0,0 +1,28 @@
+#Project 1
+--
+###JavaScript-Based Survival Game
+
+One-two players will fight to the death, destoying evil space bugs for a chance at collecting the high score. Space-Cadets will be armed with nothing but their blaster guns, which will be operated by the users arrow keys. 
+
+Each player will take turns holding off the bug monsters for as long as possible. The current player must press the corisponding arrow key at the exact time a enemy appears, to dissolve them. If missed, the monster will advance one step closer and grow more powerful. If the monster reaches you in the middle, your character will be terminated.
+
+
+**[Insert Game Screen Shot Here]**
+
+###Controls
+
+Players can only fire their blasters horizontally or vertically with the arrow keys.
+
+Spacebar to start, or restart.
+
+##To Play
+
+Visit **[Insert Website Here]** to play, or simply download a .zip of this repo and open index.html in your browser.
+
+##Development process
+
+Technologies used: HTML / CSS / JavaScript / jQuery
+
+
+
+
